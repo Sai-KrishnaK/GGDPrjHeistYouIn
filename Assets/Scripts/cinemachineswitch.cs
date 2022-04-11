71cec66d009bb2de64ac4e39cd4554b508f023ee
SaiKrishnaPrasadK <saikskp@gmail.com>
2022-04-11 18:35:28 -0500

    Cinemachine update without input system

--- Assets/Scripts/cinemachineswitch.cs
+++ Assets/Scripts/cinemachineswitch.cs
@@ -2,38 +2,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class cinemachineswitch : MonoBehaviour
 {
 
-    public Animator animator;
+    [SerializeField]
+    private InputAction action;
 
+    [SerializeField]
+    private CinemachineVirtualCamera vcam1; // pverworld
 
-    private void Awake()
+    [SerializeField]
+    private CinemachineVirtualCamera vcam2; // player
+
+  //  public Animator animator;
+
+    private bool overworldCamera = true;
+
+   
+
+    private void SwitchPriority()
     {
-        animator = GetComponent<Animator>();
+        if(overworldCamera)
+        {
+            vcam1.Priority = 0;
+            vcam2.Priority = 1;
+        }
+        else
+        {
+            vcam1.Priority = 1;
+            vcam2.Priority = 0;
+        }
+        overworldCamera = !overworldCamera;
     }
 
     private void OnEnable()
     {
-        
+        action.Enable();
     }
-
     private void OnDisable()
     {
-        
+        action.Disable();   
     }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        action.performed += _ => SwitchPriority();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
