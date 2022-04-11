60857d5858ae8dd3270e268ab90d1dd31bd9fcd8
SaiKrishnaPrasadK <saikskp@gmail.com>
2022-04-11 18:10:50 -0500

    cinemachine half
    before change of input system

---
+++ Assets/Scripts/cinemachineswitch.cs
@@ -0,0 +1,39 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class cinemachineswitch : MonoBehaviour
+{
+
+    public Animator animator;
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
